9278699b75cc768f16c3d57423f2693c5a164936
shashimahadik777 <shashimahadik777@gmail.com>
2026-02-17 20:51:48 +0530

    added validation to mail properties

---
+++ springaidemo/src/main/java/com/poc/springaidemo/MailConfig.java
@@ -0,0 +1,53 @@
+package com.poc.springaidemo;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Configuration
+@PropertySource("classpath:mail.properties")
+@ConfigurationProperties(prefix = "mail")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+public class MailConfig {
+    @NotNull
+    private String to;
+    @NotNull
+    private String from;
+    @Max(value = 40)
+    @Min(value = 20)
+    private Integer age;
+
+    @NotNull
+    private String firstname;  //loose binding
+    @NotNull
+    private String lastname; //loose binding
+    @NotNull
+    private String middlename; //loose binding
+
+    private String[] cc;
+    private List<String> bcc;
+
+    @Valid
+    private Credential credential = new Credential();
+
+    @Data
+    public class Credential {
+        private String username;
+        @Size(min = 4, max = 8)
+        private String password;
+    }
+}
