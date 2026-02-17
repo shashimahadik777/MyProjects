1174e01bf86e71d4f16eb59155f214bbfbbe096f
shashimahadik777 <shashimahadik777@gmail.com>
2026-02-17 20:38:08 +0530

    created properties file

---
+++ springaidemo/src/main/java/com/poc/springaidemo/StudentConfig.java
@@ -0,0 +1,28 @@
+package com.poc.springaidemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+//@PropertySource("classpath:student.properties")
+//@PropertySource("file:\\C:\\Training\\student1.properties")
+@PropertySources({
+        @PropertySource("classpath:student.properties"),
+        @PropertySource("file:\\C:\\Training\\student1.properties")
+})
+@ConfigurationProperties(prefix = "student")
+@Data  //@Getter+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentConfig {
+    private String name;
+    private String address;
+    private Integer age;
+    private String email;
+    private String course;
+}
